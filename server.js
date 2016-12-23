de45ce9fff134fda5025f314eb7c51480ad4e26a
sgtquezada <sgtquezada@gmail.com>
2016-12-23 13:26:29 -0800

    added post endpoint and test cases

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const {Story} = require('./models');
+const {DATABASE_URL, PORT} = require('./config');
 
 mongoose.Promise = global.Promise;
 
-const DATABASE_URL = process.env.DATABASE_URL ||
-                     global.DATABASE_URL ||
-                     'mongodb://localhost/hn-api';
-const PORT = process.env.PORT || 8080;
+// const DATABASE_URL = process.env.DATABASE_URL ||
+//                      global.DATABASE_URL ||
+//                      'mongodb://localhost/hn-api';
+// const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,25 +19,43 @@ app.get('/stories', (req, res) => {
   Story.find().limit(20).exec().then(function(stories) {
     res.status(200).json({
       stories: stories.map(
-        (story) => story.apiRepr());
+        (story) => story.apiRepr())
     });
-  });
+  })
   .catch(err => {
     return res.status(500).json({message: 'Internal Server Error'});
+  });
+});
+
+app.post('/stories', (req, res) => {
+  const requiredFields = ['title', 'url'];
+  requiredFields.forEach(field => {
+    if (!(field in req.body && req.body[field])) {
+      return res.status(400).json({message: `Must specify value for ${field}`});
+    }
+  });
+  Story.create({
+    title: req.body.title,
+    url: req.body.url
+  })
+  .then(
+    story => res.status(201).json(story.apiRepr()))
+  .catch(err => {
+    res.status(500).json({message: 'Internal Server Error'});
   });
 });
 
 
 
 let server;
-function runServer() {
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, err => {
+    mongoose.connect(databaseUrl, err => {
       if (err) {
         return reject(err);
       }
-      server = app.listen(PORT, () => {
-        console.log(`Your app is listening on port ${PORT}`);
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
         resolve();
       })
       .on('error', err => {
--- test/test-server.js
+++ test/test-server.js
@@ -2,7 +2,8 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const faker = require('faker');
-const {Story} = require('./models');
+const {Story} = require('../models');
+const {TEST_DATABASE_URL} = require('../config');
 
 const should = chai.should();
 
@@ -13,7 +14,7 @@ chai.use(chaiHttp);
 function seedData() {
   console.info('Seeding data');
   const seedData = [];
-  for (var i = 0; i <= 20; i += 1) {
+  for (var i = 0; i < 20; i += 1) {
     seedData.push(generateStoriesData());
   }
   return Story.insertMany(seedData);
@@ -34,7 +35,7 @@ function tearDownDb() {
 
 describe('Hacker News API', function() {
     before(function() {
-        return runServer();
+        return runServer(TEST_DATABASE_URL);
     });
 
     beforeEach(function() {
@@ -55,19 +56,47 @@ describe('Hacker News API', function() {
       // check response
       // fetch data from db
       // check response object matches data in db
+      let res;
       it('should return all stories', function() {
         return chai.request(app)
         .get('/stories')
-        .then(res => {
-          let responseStoryId = res.body.stories[0]._id;
-          let responseStoryAuthor = res.body.stories[0].author;
-          let storyFromDB = Story.findById(responseStoryId);
+        .then(_res => {
+          res = _res;
           res.should.have.status(200);
           res.body.stories.should.be.a('array');
-          storyFromDB.author.should.equal(responseStoryAuthor);
+          res.body.stories.should.have.length.of.at.least(1);
+          return Story.count();
+        })
+        .then(function(count) {
+          res.body.stories.should.have.length.of(count);
         });
       });
-      
+
+
 
     });
+
+    describe('POST request endpoint', function() {
+      it('should add a new story', function() {
+        const newStory = generateStoriesData();
+
+        return chai.request(app)
+        .post('/stories')
+        .send(newStory)
+        .then(function(res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.include.keys('id', 'title', 'url', 'votes');
+          res.body.id.should.not.be.null;
+          res.body.title.should.equal(newStory.title);
+          res.body.url.should.equal(newStory.url);
+          return Story.findById(res.body.id);
+        })
+        .then(function(story) {
+          story.title.should.equal(newStory.title);
+          story.url.should.equal(newStory.url);
+        })
+      })
+    })
 });
